122397b81853a140a52673ace3128cfdcb492e53
vaishudeshmukh <ladanevaishali07@gmail.com>
2023-08-29 23:38:59 +0530

    added

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.write("Hello")
+//document.write("Hello")
 console.log("My first document")
-let btn = document.getElementById("one")
-btn.addEventListener("click",function(){
-   btn.style.color = "red"
+// let btn = document.getElementById("one")
+// btn.addEventListener("click",function(){
+//    btn.style.color = "red"
    
-})
+// })
 let q = 50
 console.log(q)
